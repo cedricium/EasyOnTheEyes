7f001fee4dd725675e3fe7da60d504b9bbe9a16e
Cedricium <amaya.cedric@gmail.com>
2017-05-17 11:46:32 -0700

    Add comments outlining how overall webextension will work

--- easyOnTheEyes.js
+++ easyOnTheEyes.js
@@ -5,3 +5,15 @@ browser.alarms.create({
   delayInMinutes,
   periodInMinutes
 });
+
+
+/**
+ * Basic outline for how this webextension should work:
+ *
+ * 1. Upon opening of browser, start 20 minute countdown.
+ * 2. After 20 minutes is up, show dialogue message saying "Time to rest! Look 20 ft. away from the comp
+ *      for at least 20 secs.
+ * 3. User must start alarm once they're ready, and step 2 is repeated.
+ *
+ * NOTE: realized this is only good for people who are using the web, not for programmers
+ *    using their IDEs. Will need to think about this!
