bdb78f6c8bb342f47f4843532dfd3659996b2298
Cedricium <amaya.cedric@gmail.com>
2017-05-20 14:44:44 -0700

    Commented out code for creating notification while testing

--- easyOnTheEyes.js
+++ easyOnTheEyes.js
@@ -1,6 +1,14 @@
+// Code needed to create a bundled webpage
+var createData = {
+      type: "detached_panel",
+      url: "panel.html",
+      width: 600,
+      height: 325
+    };
+
 // Below is the code used for clicking the icon to start countdown timer (preliminary testing)
 
-function startTimer() {
+function startTimer() {  
   var delayInMinutes = 0.1;
   
   browser.alarms.create({
@@ -8,12 +16,14 @@ function startTimer() {
   });
   
   browser.alarms.onAlarm.addListener(function(alarm){
-    browser.notifications.create({
-      "type": "basic",
-      "iconUrl": "icons/timer-96.png",
-      "title": "Time for a break!",
-      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
-    });
+    var creating = browser.windows.create(createData);
+    
+//    browser.notifications.create({
+//      "type": "basic",
+//      "iconUrl": "icons/timer-96.png",
+//      "title": "Time for a break!",
+//      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
+//    });
   });
 }
 
