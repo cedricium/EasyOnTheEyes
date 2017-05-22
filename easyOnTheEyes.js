a2ee4df7ea3f52edcad8562c3cd495f5ff869f9b
Cedricium <amaya.cedric@gmail.com>
2017-05-22 11:37:14 -0700

    Removed code that initiates creation of a detached_panel (no longer needed); notification creation uncommented

--- easyOnTheEyes.js
+++ easyOnTheEyes.js
@@ -27,4 +27,23 @@ function startTimer() {
   });
 }
 
-browser.browserAction.onClicked.addListener(startTimer);
+//browser.browserAction.onClicked.addListener(startTimer);
+
+function notify() {
+  var delayInMinutes = 0.1;
+  
+  browser.alarms.create({
+    delayInMinutes
+  });
+  
+  browser.alarms.onAlarm.addListener(function(alarm){
+    browser.notifications.create({
+      "type": "basic",
+      "iconUrl": "icons/timer-96.png",
+      "title": "Time for a break!",
+      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
+    });
+  });
+}
+
+document.getElementById("startBtn").addEventListener("click", notify);
