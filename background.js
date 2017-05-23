3be657967f9094ecbdd29363e825ebb25ac1e6a9
Cedricium <amaya.cedric@gmail.com>
2017-05-22 23:59:14 -0700

    Add code to handle toggle switch

--- background.js
+++ background.js
@@ -4,7 +4,8 @@ function connected(p) {
   portFromCS = p;
   
   portFromCS.onMessage.addListener(function(m) {
-    var delayInMinutes = 1;
+    var message = m;
+    var delayInMinutes = 0.05;
 
     // setup for creating alarm (may need to use interval())
     browser.alarms.create({
@@ -14,20 +15,12 @@ function connected(p) {
     browser.alarms.onAlarm.addListener(function(alarm) {
       browser.notifications.create({
         "type": "basic",
-        "iconUrl": "icons/timer-96.png",
+        "iconUrl": "icons/monocle-96.png",
         "title": "Time for a break!",
-        "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!"
+        "message": m.message
+//        "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!"
       });
-    });
-    
-    // notification created immediately upon successful connection
-//    browser.notifications.create({
-//      "type": "basic",
-//      "iconUrl": "icons/timer-96.png",
-//      "title": "Time for a break!",
-//      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!"
-//    });
-    
+    });  
   });
 }
 
--- easyOnTheEyes.js
+++ easyOnTheEyes.js
@@ -8,15 +8,15 @@ var createData = {
 
 // Below is the code used for clicking the icon to start countdown timer (preliminary testing)
 
-function startTimer() {  
-  var delayInMinutes = 0.1;
-  
-  browser.alarms.create({
-    delayInMinutes
-  });
-  
-  browser.alarms.onAlarm.addListener(function(alarm){
-    var creating = browser.windows.create(createData);
+//function startTimer() {  
+//  var delayInMinutes = 0.1;
+//  
+//  browser.alarms.create({
+//    delayInMinutes
+//  });
+//  
+//  browser.alarms.onAlarm.addListener(function(alarm){
+//    var creating = browser.windows.create(createData);
     
 //    browser.notifications.create({
 //      "type": "basic",
@@ -24,27 +24,27 @@ function startTimer() {
 //      "title": "Time for a break!",
 //      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
 //    });
-  });
-}
+//  });
+//}
 
 //browser.browserAction.onClicked.addListener(startTimer);
 
-function notify() {
-  var delayInMinutes = 0.1;
-  
-  browser.alarms.create({
-    delayInMinutes
-  });
-  
-  browser.alarms.onAlarm.addListener(function(alarm){
-    browser.notifications.create({
-      "type": "basic",
-      "iconUrl": "icons/timer-96.png",
-      "title": "Time for a break!",
-      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
-    });
-  });
-}
+//function notify() {
+//  var delayInMinutes = 0.05;
+//  
+//  browser.alarms.create({
+//    delayInMinutes
+//  });
+//  
+//  browser.alarms.onAlarm.addListener(function(alarm){
+//    browser.notifications.create({
+//      "type": "basic",
+//      "iconUrl": "icons/timer-96.png",
+//      "title": "Time for a break!",
+//      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
+//    });
+//  });
+//}
 
 // open and attempt to make a connection
 var port = browser.runtime.connect();
@@ -55,5 +55,19 @@ function testMessenging() {
   port.postMessage({message: "Hello from easyOnTheEyes.js!"});
 }
 
+function testStop() {
+  alert('Goodbye!');
+  port.postMessage({message: "Hello from easyOnTheEyes.js!"});
+}
+
 //document.getElementById("startBtn").addEventListener("click", notify);
-document.getElementById("startBtn").addEventListener("click", testMessenging);
+//document.getElementById("startBtn").addEventListener("click", testMessenging);
+
+var toggle = document.getElementById("toggleswitch");
+toggle.addEventListener("click", function onclick(e) {
+  if (toggle.checked) {
+    testMessenging();
+  } else {
+    testStop();
+  }
+});
