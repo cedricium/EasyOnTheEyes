62c97c35ad85a7ac5cd4ee9696572a1a49a90ceb
Cedricium <amaya.cedric@gmail.com>
2017-05-28 00:53:00 -0700

    Setup communication between JS files for setting duration and stopping alarms - WIP issue #3

--- background.js
+++ background.js
@@ -3,24 +3,36 @@ var portFromCS;
 function connected(p) {
   portFromCS = p;
   
-  portFromCS.onMessage.addListener(function(m) {
-    var message = m;
-    var delayInMinutes = 0.05;
+  portFromCS.onMessage.addListener(function timer(m) {
+//    var message = m;
+    
+    if (m.message != "Cancel Alarms") {
+      var duration = m.message.duration / 60;
+      
+      var delayInMinutes = duration;
+      var periodInMinutes = duration;
 
-    // setup for creating alarm (may need to use interval())
-    browser.alarms.create({
-      delayInMinutes
-    });
+      // setup for creating alarm (may need to use interval())
+      browser.alarms.create({
+        delayInMinutes,
+        periodInMinutes
+      });
+      console.log("Alarms scheduled.");
+    } else {
+      var clearAlarms = browser.alarms.clearAll();
+      console.log("Alarms cleared.");
+    }
     
     browser.alarms.onAlarm.addListener(function(alarm) {
       browser.notifications.create({
         "type": "basic",
         "iconUrl": "icons/monocle-96.png",
         "title": "Time for a break!",
-        "message": m.message
+//        "message": m.message
+        "message": "Hopefully this works!"
 //        "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!"
       });
-    });  
+    });
   });
 }
 
--- easyOnTheEyes.js
+++ easyOnTheEyes.js
@@ -6,63 +6,45 @@ var createData = {
       height: 400
     };
 
-// Below is the code used for clicking the icon to start countdown timer (preliminary testing)
-
-//function startTimer() {  
-//  var delayInMinutes = 0.1;
-//  
-//  browser.alarms.create({
-//    delayInMinutes
-//  });
-//  
-//  browser.alarms.onAlarm.addListener(function(alarm){
-//    var creating = browser.windows.create(createData);
-    
-//    browser.notifications.create({
-//      "type": "basic",
-//      "iconUrl": "icons/timer-96.png",
-//      "title": "Time for a break!",
-//      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
-//    });
-//  });
-//}
-
-//browser.browserAction.onClicked.addListener(startTimer);
-
-//function notify() {
-//  var delayInMinutes = 0.05;
-//  
-//  browser.alarms.create({
-//    delayInMinutes
-//  });
-//  
-//  browser.alarms.onAlarm.addListener(function(alarm){
-//    browser.notifications.create({
-//      "type": "basic",
-//      "iconUrl": "icons/timer-96.png",
-//      "title": "Time for a break!",
-//      "message": "Stand up, stretch, get some coffee. Do anything except look at your computer for the next 20 seconds. Enjoy, you earned it!",
-//    });
-//  });
-//}
-
 // open and attempt to make a connection
 var port = browser.runtime.connect();
 
 function testMessenging() {
   // once connection is made, send data through port
 //  alert('Testing if this function works...');
-  port.postMessage({message: "Hello from easyOnTheEyes.js!"});
+  var duration = getSettings();
+//  port.postMessage({message: "Hello World!"});
+  port.postMessage({message: duration});
 }
 
 function testStop() {
-  alert('Goodbye!');
-  port.postMessage({message: "Hello from easyOnTheEyes.js!"});
+//  alert('Goodbye!');
+  port.postMessage({message: "Cancel Alarms"});
 }
 
 //document.getElementById("startBtn").addEventListener("click", notify);
 //document.getElementById("startBtn").addEventListener("click", testMessenging);
 
+
+function getSettings() {
+  var d = document.getElementById("slider").value;
+  
+  var settings = {
+    duration: d
+  };
+  
+  return settings;
+}
+
+
+// setting the label for the slider with the current slider's value
+var slider = document.getElementById("slider");
+slider.addEventListener("change", function() {
+  document.getElementById("label").textContent = slider.value;
+  console.log(slider.value);
+});
+
+// turning on/off the toggle switch
 var toggle = document.getElementById("toggleswitch");
 toggle.addEventListener("click", function onclick(e) {
   if (toggle.checked) {
